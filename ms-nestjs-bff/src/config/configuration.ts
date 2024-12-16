f2f5ee026f95197516c883bc3838c66f4d1e6c91
ignacioArs <arcosss96@gmail.com>
2024-12-16 00:16:51 -0300

    subiendo mejoras para ms-nestjs-bff y ms-nestjs-security

--- ms-nestjs-bff/src/config/configuration.ts
+++ ms-nestjs-bff/src/config/configuration.ts
@@ -1,7 +1,23 @@
 export default () => ({                                                                            // SI NO ES DESARROLLO O PRODUCCION                                            
+    port:3002,
+    cors:'http://localhost:3003',
+    secret:process.env.JWT_SECRET,
+    msSecurity:'http://localhost:3001' ,
+    msPython:'http://localhost:3000'
+}); 
+
+/* export default () => ({                                                                            // SI NO ES DESARROLLO O PRODUCCION                                            
     port: process.env.ENTORNO_ENV ==='produccion'? parseInt(process.env.PORT_PRODUCCION, 10) : parseInt(process.env.PORT_DESARROLLO, 10) || 3002,
     cors:process.env.ENTORNO_ENV ==='produccion'? process.env.CORS_ORIGIN_PRODUCCION : process.env.CORS_ORIGIN_DESARROLLO || 'http://localhost:3003',
     secret:process.env.JWT_SECRET,
     msSecurity:process.env.ENTORNO_ENV ==='produccion'? process.env.MS_SECURITY_ORIGIN_PRODUCCION: process.env.MS_SECURITY_ORIGIN_DESARROLLO || 'http://localhost:3001' ,
     msPython:process.env.ENTORNO_ENV ==='produccion'? process.env.MS_PYTHON_ORIGIN_PRODUCCION: process.env.MS_PYTHON_ORIGIN_DESARROLLO || 'http://localhost:3000'
-  });
+}); */
+
+/* export default () => ({                                                                            // SI NO ES DESARROLLO O PRODUCCION                                            
+    port: process.env.ENTORNO_ENV ==='produccion'? parseInt(process.env.PORT_PRODUCCION, 10) : parseInt(process.env.PORT_DESARROLLO, 10) || 3002,
+    cors:process.env.ENTORNO_ENV ==='produccion'? process.env.CORS_ORIGIN_PRODUCCION : process.env.CORS_ORIGIN_DESARROLLO || 'http://localhost:3003',
+    secret:process.env.JWT_SECRET,
+    msSecurity:process.env.ENTORNO_ENV ==='produccion'? process.env.MS_SECURITY_ORIGIN_PRODUCCION: process.env.MS_SECURITY_ORIGIN_DESARROLLO || 'http://localhost:3001' ,
+    msPython:process.env.ENTORNO_ENV ==='produccion'? process.env.MS_PYTHON_ORIGIN_PRODUCCION: process.env.MS_PYTHON_ORIGIN_DESARROLLO || 'http://localhost:3000'
+}); */
--- ms-nestjs-security/src/config/configuration.ts
+++ ms-nestjs-security/src/config/configuration.ts
@@ -1,4 +1,18 @@
 export default () => ({
+  port:3001,
+  cors:'http://localhost:3002',
+  secret:process.env.JWT_SECRET,
+  database: {
+    host:'localhost'  ,
+    port:5432,
+    username:'admin',
+    password:'admin',
+    name:'proyecto',
+  }
+});
+
+
+/* export default () => ({
     port: process.env.ENTORNO_ENV ==='produccion'? parseInt(process.env.PORT_PRODUCCION, 10) : parseInt(process.env.PORT_DESARROLLO, 10) || 3001,
     cors:process.env.ENTORNO_ENV ==='produccion'? process.env.CORS_ORIGIN_PRODUCCION : process.env.CORS_ORIGIN_DESARROLLO || 'http://localhost:3002',
     secret:process.env.JWT_SECRET,
@@ -9,5 +23,18 @@ export default () => ({
       password: process.env.ENTORNO_ENV ==='produccion'? process.env.DB_PASSWORD_PRODUCCION : process.env.DB_PASSWORD_DESARROLLO || 'admin',
       name: process.env.ENTORNO_ENV ==='produccion'? process.env.DB_NAME_PRODUCCION : process.env.DB_NAME_DESARROLLO || 'proyecto',
     }
-  });
+  }); */
   
+
+  /* export default () => ({
+    port: process.env.ENTORNO_ENV ==='produccion'? parseInt(process.env.PORT_PRODUCCION, 10) : parseInt(process.env.PORT_DESARROLLO, 10) || 3001,
+    cors:process.env.ENTORNO_ENV ==='produccion'? process.env.CORS_ORIGIN_PRODUCCION : process.env.CORS_ORIGIN_DESARROLLO || 'http://localhost:3002',
+    secret:process.env.JWT_SECRET,
+    database: {
+      host: process.env.ENTORNO_ENV ==='produccion'? process.env.DB_HOST_PRODUCCION : process.env.DB_HOST_DESARROLLO || 'localhost'  ,
+      port: process.env.ENTORNO_ENV ==='produccion'? parseInt(process.env.DB_PORT_PRODUCCION) : parseInt(process.env.DB_PORT_DESARROLLO) || 5432,
+      username: process.env.ENTORNO_ENV ==='produccion'? process.env.DB_USERNAME_PRODUCCION : process.env.DB_USERNAME_DESARROLLO || 'admin',
+      password: process.env.ENTORNO_ENV ==='produccion'? process.env.DB_PASSWORD_PRODUCCION : process.env.DB_PASSWORD_DESARROLLO || 'admin',
+      name: process.env.ENTORNO_ENV ==='produccion'? process.env.DB_NAME_PRODUCCION : process.env.DB_NAME_DESARROLLO || 'proyecto',
+    }
+  }); */
