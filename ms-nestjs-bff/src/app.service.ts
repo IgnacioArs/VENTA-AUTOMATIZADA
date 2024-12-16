72925bc04fad33c078b2a6b97edfe9b63b167ffa
ignacioArs <arcosss96@gmail.com>
2024-12-16 02:38:58 -0300

    commit- se realizan nuevos cambios para pipeline de jenkins Hellow World!

--- ms-nestjs-bff/src/app.service.ts
+++ ms-nestjs-bff/src/app.service.ts
@@ -6,6 +6,7 @@ export class AppService {
   constructor(private configService:ConfigService){}
   getHello(): string {
     const port = this.configService.get<number>('port');
-    return 'Hello World! SERVER RUNNING PORT:'+`${port? port:3002}`;
+    /* return 'Hello World! SERVER RUNNING PORT:'+`${port}`; */
+    return 'Hello World!';
   }
 }
