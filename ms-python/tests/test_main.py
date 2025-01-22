7d9e4d87a70f5a1593aad78948a07ec9c8af6581
ignacioArs <arcosss96@gmail.com>
2025-01-22 20:26:52 -0300

    commit- Se realiza nuevo pipline ajustando las capas

--- ms-python/tests/test_main.py
+++ ms-python/tests/test_main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fastapi.testclient import TestClient
 from main.app import app  # Asegúrate de que esta ruta sea válida en tu proyecto
 
