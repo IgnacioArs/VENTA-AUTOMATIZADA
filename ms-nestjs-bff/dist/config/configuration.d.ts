16d113bb8fa0eac295150efbceb1e198ec7319ee
ignacioArs <arcosss96@gmail.com>
2025-03-06 07:48:32 -0300

    commit- commit cambio de version nodejs

--- ms-nestjs-bff/dist/config/configuration.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: () => {
-    port: number;
-    cors: string;
-    secret: string;
-    msSecurity: string;
-    msPython: string;
-};
-export default _default;
--- ms-nestjs-security/dist/modules/auth/entities/userInterface.d.ts
+++
@@ -1,5 +0,0 @@
-import { UserEntity } from "src/modules/users/entities/user.entity";
-export interface AuthResponse {
-    user: UserEntity;
-    token: string;
-}
